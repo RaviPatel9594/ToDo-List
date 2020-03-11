a4f609b504ff777406a36178c0ac788ea772b2e9
Ravi Patel <ravipatel9594@gmail.com>
2020-03-11 23:23:50 +0530

    Updates

---
+++ precache-manifest.c8ad281d26bcfeced2096dad7b042bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0867cab2a96e4e19a256f6df1b493c",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "2c800de17afb6b7d55be",
+    "url": "/Todo-List/static/css/main.38007978.chunk.css"
+  },
+  {
+    "revision": "d7c79055151d0ba9d457",
+    "url": "/Todo-List/static/js/2.dbf6194c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-List/static/js/2.dbf6194c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c800de17afb6b7d55be",
+    "url": "/Todo-List/static/js/main.c8990350.chunk.js"
+  },
+  {
+    "revision": "7554fc52b30076c28734",
+    "url": "/Todo-List/static/js/runtime-main.249f1496.js"
+  }
+]);
